166de853333f18cbcf9565f0c8272e0af98e211e
PieterNatanael <pieternatanael@icloud.com>
2025-03-11 10:42:02 +0800

    Initial Commit

---
+++ toko/tokoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tokoApp.swift
+//  toko
+//
+//  Created by Pieter Yoshua Natanael on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct tokoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ tokoTests/tokoTests.swift
@@ -0,0 +1,17 @@
+//
+//  tokoTests.swift
+//  tokoTests
+//
+//  Created by Pieter Yoshua Natanael on 11/03/25.
+//
+
+import Testing
+@testable import toko
+
+struct tokoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
